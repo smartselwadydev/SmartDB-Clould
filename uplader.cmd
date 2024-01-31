f8189263a8d98d87b4dc18ba2476120885c8b0f7
unknown <media@smartselwady.com>
2024-01-31 17:05:22 +0300

    first commit

--- uplader.cmd
+++ uplader.cmd
@@ -1,6 +1,6 @@
-git init
+git status
 git add .
 git commit -m "first commit"
-git branch -M main
 git remote add origin https://github.com/smartselwadydev/SmartDB-Clould.git
+git branch -M main
 git push -u origin main
