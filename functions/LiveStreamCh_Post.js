ef81e6a30f9879ca2abe5c45063daaaaa53b655c
unknown <media@smartselwady.com>
2024-01-31 17:44:31 +0300

    first commit

--- functions/LiveStreamCh_Post.js
+++ functions/LiveStreamCh_Post.js
@@ -4,16 +4,18 @@ export async function onRequestPost(context) {
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-        }
+        },
+		body: JSON.stringify({api: 'LiveStreamCh'})
+		
     };
 
-  try {
-    const fetchResponse = await fetch(`https://panel.smartselwady.com/YallaGoal/updatenews1.php`, settings);
-    const data = await fetchResponse.json();
-	let postData = JSON.stringify(data, null, 2);
-	const database = await context.env.NORTHWIND_DB.prepare("UPDATE LiveStreamCh SET fetchDB = ?1 WHERE id = ?2").bind( postData , 1 ).run()
-	return new Response('Success parsing content');
-  } catch (err) {
-    return new Response('Error parsing content', { status: 400 });
-  }
+    try {
+		const fetchResponse = await fetch(`https://panel.smartselwady.com/SmartDB-sync.php`, settings);
+		const data = await fetchResponse.json();
+		let postData = JSON.stringify(data, null, 2);
+		const database = await context.env.NORTHWIND_DB.prepare("UPDATE LiveStreamCh SET fetchDB = ?1 WHERE id = ?2").bind( postData , 1 ).run()
+		return new Response('Success parsing content');
+    } catch (err) {
+		return new Response('Error parsing content', { status: 400 });
+    }
 }
