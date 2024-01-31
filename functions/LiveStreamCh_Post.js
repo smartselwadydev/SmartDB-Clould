66e0b3698a27089eacb7c38793070c7e1399b384
unknown <media@smartselwady.com>
2024-01-31 17:11:20 +0300

    first commit

--- functions/LiveStreamCh_Post.js
+++ functions/LiveStreamCh_Post.js
@@ -12,11 +12,7 @@ export async function onRequestPost(context) {
     const data = await fetchResponse.json();
 	let postData = JSON.stringify(data, null, 2);
 	const database = await context.env.NORTHWIND_DB.prepare("UPDATE LiveStreamCh SET fetchDB = ?1 WHERE id = ?2").bind( postData , 1 ).run()
-	return new Response(postData, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
+	return new Response('Success parsing content');
   } catch (err) {
     return new Response('Error parsing content', { status: 400 });
   }
