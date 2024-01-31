f77917de6dee61032076923016443642cbb66888
unknown <media@smartselwady.com>
2024-01-31 18:00:48 +0300

    first commit

---
+++ functions/LiveStreamCh_View.js
@@ -0,0 +1,13 @@
+
+export async function onRequest(context) {
+  const corsHeaders = {
+	  'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Max-Age': '86400',
+  }
+  const results = context.env.NORTHWIND_DB.prepare('SELECT * from LiveStreamCh WHERE id = ?').bind( 1 );
+  const data = await results.all();
+  return Response.json(data, {
+      headers: corsHeaders
+  });
+}
