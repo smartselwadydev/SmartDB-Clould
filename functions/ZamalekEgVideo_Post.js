8aa3f9f51f7f3eca029023fb91f315f2a5259f09
unknown <media@smartselwady.com>
2024-05-09 11:14:46 +0300

    first commit

---
+++ functions/ZamalekEgVideo_Post.js
@@ -0,0 +1,19 @@
+export async function onRequestPost(context) {
+	const settings = {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        }
+    };
+
+    try {
+		const fetchResponse = await fetch(`https://zamalek-eg.com/customApi/ZamalekEgVideo.php`, settings);
+		const data = await fetchResponse.json();
+		let postData = JSON.stringify(data, null, 2);
+		const database = await context.env.NORTHWIND_DB.prepare("UPDATE ZamalekEgVideo SET fetchDB = ?1 WHERE id = ?2").bind( postData , 1 ).run()
+		return new Response('Success parsing content');
+    } catch (err) {
+		return new Response('Error parsing content', { status: 400 });
+    }
+}
