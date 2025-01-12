f2a9e8c9bb818000a29e569c0684e1a3a02346a6
unknown <media@smartselwady.com>
2025-01-12 23:19:14 +0300

    first commit

--- flutter_apps/LiveSpot/app.js
+++ flutter_apps/LiveSpot/app.js
@@ -9,54 +9,36 @@ function removeElementsExceptClass(className) {
 }
 				
 function checkUrl(url) {
-    if (url.includes("shahid.mbc.net")) {
-        removeElementsExceptClass('xxxx');
-                let scrollTop, scrollLeft;
-                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-                window.onscroll = function() {
-                    window.scrollTo(scrollLeft, scrollTop);
-                };
-                const centerElements = document.querySelectorAll('center');
-                centerElements.forEach(element => {
-                    element.remove();
-                });
-                const hrElements = document.querySelectorAll('hr');
-                hrElements.forEach(element => {
-                    element.remove();
-                });
-                const playerDiv = document.createElement('div');
-                playerDiv.id = 'player';
-                playerDiv.style.position = 'absolute';
-                playerDiv.style.top = '0px';
-                playerDiv.style.bottom = '0px';
-                playerDiv.style.right = '0px';
-                playerDiv.style.left = '0px';
-                document.body.appendChild(playerDiv);
-                const script = document.createElement('script');
-                script.src = 'https://cdn.jsdelivr.net/npm/clappr@latest/dist/clappr.min.js';
-                script.onload = function() {
-                        var player = new Clappr.Player({
-                            source: 'https://ssc-news-live-enc.edgenextcdn.net/out/v1/9002bae7e2a34317bbaf239489960a8a/index_12.m3u8',
-                            parentId: '#player',
-                            autoPlay: true,
-                            height: '100vh',
-                            width: '100vw',
-                            mute: false,
-                            mediacontrol: {
-                                seekbar: '#000000',
-                                buttons: '#FFF'
-                            },
-                            playback: {
-                                hlsjsConfig: {
-                                    maxBufferSize: 0,
-                                    maxBufferLength: 7,
-                                    liveSyncDurationCount: 7,
-                                }
-                            }
-                        });
-                };
-                document.body.appendChild(script);		
+    if (url.includes("azrotv.com")) {
+		document.body.innerHTML = '';
+		document.body.innerHTML = `
+			<div id="player"></div>
+			<div id="error-message">Unsupported browser. Try Google Chrome.</div>
+			<script>
+				if (Hls.isSupported() && p2pml.hlsjs.Engine.isSupported()) {
+					var engine = new p2pml.hlsjs.Engine();
+					var player = jwplayer("player");
+					player.setup({
+						playlist: [{
+							"sources": [{
+								"file": atob("aHR0cHM6Ly9zc2MtMS1lbmMuZWRnZW5leHRjZG4ubmV0L291dC92MS9jNjk2ZTQ4MTliNTU0MTQzODhhMWE0ODdlOGE0NWNhMS9pbmRleC5tcGQ="),
+							}]
+						}],
+						width: "100%",
+						height: "100%",
+						autostart: true,
+						cast: {}
+					});
+					jwplayer_hls_provider.attach();
+					p2pml.hlsjs.initJwPlayer(player, {
+						liveSyncDurationCount: 7,
+						loader: engine.createLoaderClass(),
+					});
+				} else {
+					document.write("Unsupported browser. Try Google Chrome");
+				}
+			</script>
+		`;
     }
 }
 checkUrl(page_url);
