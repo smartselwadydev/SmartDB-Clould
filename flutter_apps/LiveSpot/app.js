e3d82e2c8cfa92aca1b0f928e0267e40fd9c763d
unknown <media@smartselwady.com>
2025-01-12 23:50:00 +0300

    first commit

--- flutter_apps/LiveSpot/app.js
+++ flutter_apps/LiveSpot/app.js
@@ -1,14 +1,12 @@
 
 function checkUrl(url) {
     if (url.includes("azrotv.com")) {
-alert(4);
-		document.getElementById('messageBox').remove();
-		document.getElementById('laoad_Adbl').remove();
-		document.getElementById('laoad_Adb2').remove();
-		document.querySelector('iframe').remove();
-		alert(0);
-
-
+		setTimeout(function () {
+				document.getElementById('messageBox').remove();
+				document.getElementById('laoad_Adbl').remove();
+				document.getElementById('laoad_Adb2').remove();
+				document.querySelector('iframe').remove();
+		},1000); 
     }
 }
 checkUrl(page_url);
