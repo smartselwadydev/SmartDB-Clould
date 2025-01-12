e278a2cb74368615e7ad20f8baa0646bbeafe547
unknown <media@smartselwady.com>
2025-01-12 23:44:36 +0300

    first commit

--- flutter_apps/LiveSpot/app.js
+++ flutter_apps/LiveSpot/app.js
@@ -1,28 +1,7 @@
-function removeElementsExceptClass(className) {
-    const bodyChildren = document.body.children;
-    const bodyChildrenArray = Array.from(bodyChildren);
-    bodyChildrenArray.forEach(child => {
-        if (!child.classList.contains(className)) {
-            child.remove();
-        }
-    });
-}
-function removeScriptWithFunction(functionName) {
-        const scripts = document.getElementsByTagName('script');
-        for (let i = scripts.length - 1; i >= 0; i--) {
-            if (scripts[i].innerHTML.includes(functionName)) {
-                scripts[i].parentNode.removeChild(scripts[i]);
-            }
-        }
-}
-	
+
 function checkUrl(url) {
     if (url.includes("azrotv.com")) {
-		var script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js'; 
-		document.getElementsByTagName('body')[0].appendChild(script);
-		
+alert(4);
 		document.getElementById('messageBox').remove();
 		document.getElementById('laoad_Adbl').remove();
 		document.getElementById('laoad_Adb2').remove();
@@ -30,9 +9,9 @@ function checkUrl(url) {
 		removeScriptWithFunction('myFunctionShow');
 		removeScriptWithFunction('_Hasync');
 		removeScriptWithFunction('undefined');
-		alert(0)
+		alert(0);
 		$('.message-box').remove()
-		alert(1)
+		alert(1);
 		
 
     }
