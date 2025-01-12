9f621155fb0234c400efa6b57d502b0a7cb50543
unknown <media@smartselwady.com>
2025-01-12 23:26:25 +0300

    first commit

--- flutter_apps/LiveSpot/app.js
+++ flutter_apps/LiveSpot/app.js
@@ -10,35 +10,9 @@ function removeElementsExceptClass(className) {
 				
 function checkUrl(url) {
     if (url.includes("azrotv.com")) {
-		document.body.innerHTML = '';
-		document.body.innerHTML = `
-			<div id="player"></div>
-			<div id="error-message">Unsupported browser. Try Google Chrome.</div>
-			<script>
-				if (Hls.isSupported() && p2pml.hlsjs.Engine.isSupported()) {
-					var engine = new p2pml.hlsjs.Engine();
-					var player = jwplayer("player");
-					player.setup({
-						playlist: [{
-							"sources": [{
-								"file": atob("aHR0cHM6Ly9zc2MtMS1lbmMuZWRnZW5leHRjZG4ubmV0L291dC92MS9jNjk2ZTQ4MTliNTU0MTQzODhhMWE0ODdlOGE0NWNhMS9pbmRleC5tcGQ="),
-							}]
-						}],
-						width: "100%",
-						height: "100%",
-						autostart: true,
-						cast: {}
-					});
-					jwplayer_hls_provider.attach();
-					p2pml.hlsjs.initJwPlayer(player, {
-						liveSyncDurationCount: 7,
-						loader: engine.createLoaderClass(),
-					});
-				} else {
-					document.write("Unsupported browser. Try Google Chrome");
-				}
-			</script>
-		`;
+		document.getElementById('messageBox').remove();
+		document.querySelector('iframe').remove();
+
     }
 }
 checkUrl(page_url);
