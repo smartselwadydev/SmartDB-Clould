3d27c51fe7f627af08899bddc8ec0a23686e24a3
unknown <media@smartselwady.com>
2025-01-20 00:36:53 +0300

    first commit

---
+++ functions/YallaVamosNews_View.js
@@ -0,0 +1,12 @@
+export async function onRequest(context) {
+  const corsHeaders = {
+	  'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Max-Age': '86400',
+  }
+  const results = context.env.NORTHWIND_DB.prepare('SELECT * from YallaVamosNews WHERE id = ?').bind( 1 );
+  const data = await results.all();
+  return Response.json(data, {
+      headers: corsHeaders
+  });
+}
