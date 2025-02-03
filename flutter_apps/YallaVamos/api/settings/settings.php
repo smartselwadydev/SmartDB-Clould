5a735c9b0fea444663ffac31e2d1e9dac21ee12d
unknown <media@smartselwady.com>
2025-02-03 21:17:33 +0300

    first commit

--- flutter_apps/YallaVamos/api/settings/settings.php
+++ flutter_apps/YallaVamos/api/settings/settings.php
@@ -1374,9 +1374,7 @@
             "title":"Title",
             "url":"url",
             "icon":"about_item_1.png",
-            "status":"1",
-            "created_at":"2022-04-01 00:36:55",
-            "updated_at":"2022-04-01 00:36:55",
+            "status":"1"
             "translation":[
                {
                   "id":"1",
@@ -1384,9 +1382,7 @@
                   "title":"Title",
                   "description":"FlyWeb is a universal convertor Web to App inspired by the Flutter framework, made by Google. It is the best solutions to convert your website to Android and iOS with just your URL.",
                   "lang":"en",
-                  "status":"1",
-                  "created_at":"2022-12-25 13:24:09",
-                  "updated_at":"2022-12-25 13:24:09"
+                  "status":"1"
                },
                {
                   "id":"2",
@@ -1394,9 +1390,7 @@
                   "title":"Title",
                   "description":"FlyWeb \u0639\u0628\u0627\u0631\u0629 \u0639\u0646 \u0628\u0631\u0646\u0627\u0645\u062c \u062a\u062d\u0648\u064a\u0644 \u0639\u0627\u0644\u0645\u064a \u0645\u0646 \u0627\u0644\u0648\u064a\u0628 \u0625\u0644\u0649 \u062a\u0637\u0628\u064a\u0642 \u0645\u0633\u062a\u0648\u062d\u0649 \u0645\u0646 \u0625\u0637\u0627\u0631 \u0639\u0645\u0644 Flutter \u0627\u0644\u0630\u064a \u062a\u0635\u0646\u0639\u0647 Google. \u0625\u0646\u0647\u0627 \u0623\u0641\u0636\u0644 \u0627\u0644\u062d\u0644\u0648\u0644 \u0644\u062a\u062d\u0648\u064a\u0644 \u0645\u0648\u0642\u0639 \u0627\u0644\u0648\u064a\u0628 \u0627\u0644\u062e\u0627\u0635 \u0628\u0643 \u0625\u0644\u0649 Android \u0648 iOS \u0628\u0627\u0633\u062a\u062e\u062f\u0627\u0645 \u0639\u0646\u0648\u0627\u0646 URL \u0627\u0644\u062e\u0627\u0635 \u0628\u0643 \u0641\u0642\u0637.",
                   "lang":"ar",
-                  "status":"1",
-                  "created_at":"2022-07-24 18:13:34",
-                  "updated_at":"2022-07-24 18:13:34"
+                  "status":"1"
                }
             ]
          }
@@ -1404,23 +1398,19 @@
       "applicationTranslation":[
          {
             "id":"1",
-            "title":"يلا فاموس",
+            "title":"",
             "sub_title":"",
             "url":"https:\/\/yalla-vamos.online\/",
             "lang":"en",
-            "status":"1",
-            "created_at":"2025-01-24 23:59:27",
-            "updated_at":"2025-01-24 23:59:27"
+            "status":"1"
          },
          {
             "id":"10",
-            "title":"يلا فاموس",
+            "title":"",
             "sub_title":"",
             "url":"https:\/\/yalla-vamos.online\/",
             "lang":"ar",
-            "status":"1",
-            "created_at":"2025-01-24 22:00:50",
-            "updated_at":"2025-01-24 22:00:50"
+            "status":"1"
          }
       ]
    }
