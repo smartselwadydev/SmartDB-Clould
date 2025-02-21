92b197d7969f1ff5cbf4f3ee86f38a97233ce06a
unknown <media@smartselwady.com>
2025-02-21 17:41:54 +0300

    first commit

--- functions/RedLineSlide_Post.js
+++ functions/RedLineSlide_Post.js
@@ -8,7 +8,7 @@ export async function onRequestPost(context) {
     };
 
     try {
-		const fetchResponse = await fetch(`https://panel.smartselwady.com/SmartDB-sync/RedLineSlide.php`, settings);
+		const fetchResponse = await fetch(`https://yalla-vamos.online/customApi/RedLineSlide.php`, settings);
 		const data = await fetchResponse.json();
 		let postData = JSON.stringify(data, null, 2);
 		const database = await context.env.NORTHWIND_DB.prepare("UPDATE RedLineSlide SET fetchDB = ?1 WHERE id = ?2").bind( postData , 1 ).run()
