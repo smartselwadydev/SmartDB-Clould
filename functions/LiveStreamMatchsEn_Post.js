a65bb9326bda88b8c75d88bafd8e518625812d9d
unknown <media@smartselwady.com>
2025-04-12 13:53:25 +0300

    first commit

--- functions/LiveStreamMatchsEn_Post.js
+++ functions/LiveStreamMatchsEn_Post.js
@@ -8,7 +8,7 @@ export async function onRequestPost(context) {
     };
 
     try {
-		const fetchResponse = await fetch(`https://panel.smartselwady.com/SmartDB-sync/LiveStreamMatchsEn.php`, settings);
+		const fetchResponse = await fetch(`https://yalla-vamos.online/customApi/LiveStreamMatchsEn.php`, settings);
 		const data = await fetchResponse.json();
 		let postData = JSON.stringify(data, null, 2);
 		const database = await context.env.NORTHWIND_DB.prepare("UPDATE LiveStreamMatchs SET fetchDB = ?1 WHERE id = ?2").bind( postData , 2 ).run()
