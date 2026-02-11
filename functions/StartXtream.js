37f6ccf766e3854cbcb3653da552f24bedde9967
unknown <media@smartselwady.com>
2026-02-12 01:29:52 +0300

    first commit

--- functions/StartXtream.js
+++ functions/StartXtream.js
@@ -1,8 +1,8 @@
+// functions/StartXtream.js or functions/StartXtream/index.js
 export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
-  const params = Object.fromEntries(url.searchParams);
-  const ch = params["ch"];
+  const ch = url.searchParams.get("ch");
 
   if (!ch) {
     return new Response("Missing 'ch' parameter", { status: 400 });
@@ -17,7 +17,9 @@ export async function onRequest(context) {
     
     return new Response(m3u8Content, {
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/vnd.apple.mpegurl",
+        "Cache-Control": "public, max-age=60"
       }
     });
   } catch (error) {
